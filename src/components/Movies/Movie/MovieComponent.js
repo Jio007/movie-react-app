07211b9c4c13dec338f7a7e916d1211585b635a4
Jiovanny Andres Ibargüen <jiovanny007@gmail.com>
2019-01-12 17:28:35 -0500

    initial commit

---
+++ src/components/Movies/Movie/MovieComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Movie.css';
+
+class MovieComponent extends Component {
+  render() {
+    return (
+      <div className='movie'>
+        <p> Movie Component! </p>
+      </div>
+    )
+  }
+}
+
+export default MovieComponent;
---
+++ src/components/Movies/MoviesComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Movies.css';
+
+class MoviesComponent extends Component {
+  render() {
+    return (
+      <div className='movies'>
+        <p> Movies Component!! </p>
+      </div>
+    )
+  }
+}
+
+export default MoviesComponent;
