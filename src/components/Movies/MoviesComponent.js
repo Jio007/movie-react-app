48bc38e5daa437317b39889bdcdb6b944e6ca072
Jiovanny Andres Ibargüen <jiovanny007@gmail.com>
2019-01-12 18:50:30 -0500

    getting movies data

--- src/components/Movies/MoviesComponent.js
+++ src/components/Movies/MoviesComponent.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import './Movies.css';
 
 class MoviesComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        // Define the zipcode property into the component state
+        this.state = {
+            movies: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=089dde2396217b62fb377d77459dbfa9')
+            .then(data => {
+                this.setState({
+                    movies: data.data.results
+                });
+            });
+    }
+  
   render() {
+      const { movies } = this.state;
     return (
       <div className='movies'>
-        <p> Movies Component!! </p>
+        {movies.map(movie =>
+            <p>{movie.title} </p>
+        )}
       </div>
     )
   }
